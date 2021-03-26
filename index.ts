3285c2f86ba00c81330ea257ee31ca4571f70772
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-27 02:55:29 +0900

    add type to object

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-let hasValue: boolean = true;
+let hasValue = true;
 let count: number = 10;
 let float: number = 3.14;
 let negative: number = -0.12
 let single: string = 'hello';
 let double: string = `hello`;
+
+const person = {
+  name: 'Jack',
+  age: 21
+}
