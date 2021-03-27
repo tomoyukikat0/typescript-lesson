23c28f96f0719fd1d6ba85db73c76331e504458d
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-28 03:02:18 +0900

    create class

--- dist/class.js
+++ dist/class.js
@@ -3,7 +3,11 @@ var Person = /** @class */ (function () {
     function Person(initName) {
         this.name = initName;
     }
+    Person.prototype.greeting = function () {
+        console.log("Hello! My name is " + this.name);
+    };
     return Person;
 }());
 var quill = new Person('Quill');
+quill.greeting();
 console.log(quill);
--- src/class.ts
+++ src/class.ts
@@ -3,7 +3,12 @@ class Person {
 		constructor(initName: string) {
 			this.name = initName
 		}
+
+		greeting(this: { name: string }) {
+			console.log(`Hello! My name is ${this.name}`)
+		}
 }
 
 const quill = new Person('Quill');
+quill.greeting();
 console.log(quill)
